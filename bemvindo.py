31f5c881ba77a74ca2009f2de4906aafc4247254
Armando Soares Sousa <armando.sousa@gmail.com>
2024-03-13 13:34:07 +0000

    Identifica o so e mostra a data local.

--- bemvindo.py
+++ bemvindo.py
@@ -1,10 +1,20 @@
 import sys
 import datetime
+import os
 
+my_so = sys.platform
 versao_python = sys.version
 data_hoje_local = datetime.datetime.today()
 
-nome = input("Digite seu nome: ")
+if my_so.startswith("linux") or my_so.startswith("darwin"):
+    os.system("clear")
+else:
+    os.system("cls")
+
 print(f"Data: {data_hoje_local}")
+
+nome = input("Digite seu nome: ")
+
 print(f"Olá {nome}, bem vindo ao Python!")
 print(f"Este programa foi feito em Python na versão: {versao_python}")
+print(f"Sistema Operacional: {my_so}")
